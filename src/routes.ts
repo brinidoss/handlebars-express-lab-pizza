bf4a24670eeabd902e73fb50d5121f58070cd0b8
Brittany Doss <brinidoss@gmail.com>
2021-07-03 17:20:30 -0400

    added pricing

--- src/routes.ts
+++ src/routes.ts
@@ -58,10 +58,10 @@ routes.get("/build-your-own", (req, res) => {
 
 routes.post('/build-your-own/confirmation', (req, res) => {
     const size: string = req.body.size ? String(req.body.size) : '';
-    //let toppings = req.body.toppings === 'true' ? 
+    let chosenToppings: string[] = req.body.toppings;
     let glutenFree: boolean | string = req.body['gluten-free'] === 'true' ? true : false;
     const specialInstructions: string = req.body['special-instructions'] ? String(req.body['special-instructions']) : '';
-    const total: number = 10;
+    //const total: number = 0;
     
     if (glutenFree) {
         glutenFree = "Yes";
@@ -69,31 +69,48 @@ routes.post('/build-your-own/confirmation', (req, res) => {
         glutenFree = "No";
     }
 
-    // for ( const topping of toppings ) {
-        //req.body.toppings ? String(req.body.size) : '';
-        if (req.body.toppings === 'true') {
+    function calcTotal(): number {
+        let total: number = 0;
 
-        toppings.push(req.body.toppings);
-        return toppings;
+        if (String(req.body.size) === "Small") {
+            total += 7;
+            if (chosenToppings) {
+                total += chosenToppings.length * .5;
+            }
+        } else if (String(req.body.size) === "Medium") {
+            total += 10;
+            if (chosenToppings) {
+                total += chosenToppings.length;
+            }
+        } else {
+            total += 12;
+            if (chosenToppings) {
+                total += chosenToppings.length * 1.25;
+            }
         }
 
-   // }
-
+        if (glutenFree === 'Yes') {
+            total += 2;
+        }
+        return total;
+    }
+    console.log(calcTotal());
+    //console.log(chosenToppings.length);
 
     interface BuiltPizza {
         size: string;
-        toppings: any[];
+        toppings: string[];
         glutenFree: boolean | string;
         specialInstructions: string;
-        price: number,
+        price: number;
     }
 
     const newBuiltPizza: BuiltPizza = {
         size: size,
-        toppings: toppings,
+        toppings: chosenToppings,
         glutenFree: glutenFree,
         specialInstructions: specialInstructions,
-        price: total
+        price: calcTotal()
     }
 
     res.render('build-submission', {pizza: newBuiltPizza});
@@ -101,3 +118,18 @@ routes.post('/build-your-own/confirmation', (req, res) => {
 
 export default routes;
 
+
+
+
+
+// Can't believe I had all this when all I needed to do was set toppings equal to req.body.toppings...
+//     for ( const topping of toppings ) {
+//         //req.body.toppings ? String(req.body.size) : '';
+//         if (String(req.body.toppings).includes(topping)) {
+
+//         chosenToppings.push(String(req.body.toppings));
+//         console.log(chosenToppings);
+        
+//         }
+//         //return toppings;
+//    }
